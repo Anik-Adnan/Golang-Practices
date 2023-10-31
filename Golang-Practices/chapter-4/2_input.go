af8f1900d67d18c9c986794ce6a3ef404e10f229
Anik Adnan <anikadnanict17@gmail.com>
2023-10-31 18:12:30 +0600

    upload some example

--- Golang-Practices/chapter-4/2_input.go
+++ Golang-Practices/chapter-4/2_input.go
@@ -1,20 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("enter your firstName")
 	var name string
-	fmt.Scan(&name) // one word
-
-	fmt.Println("enter your age")
 	var age int
-	fmt.Scan(&age)
-	
-	fmt.Printf("FirstName : %v Age: %v", name, age)
-	fmt.Println("enter your full name:")
+
+	fmt.Println("Enter your name and age:")
+	fmt.Scan(&name, &age)
+
+	fmt.Printf("Hello, %s! You are %d years old.\n", name, age)
+
+	fmt.Println("Enter your name and age in the format \"name age\":")
+	fmt.Scanf("%s %d", &name, &age)
+	fmt.Printf("Hello, %s! You are %d years old.\n", name, age)
+
 	var fullname string
-	fmt.Scanln()
-	fmt.Scanln(&fullname) // one line
-	fmt.Printf("Your full name: %v", fullname)
+	fmt.Println("Enter your name:")
+	fmt.Scanln(&fullname)
+
+	fmt.Printf("Hello, %s!\n", fullname)
+
 }
